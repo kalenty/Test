a8f275845df84c0ff91cf8b817dbbb6c861044a7
kalenty <kalenty@outlook.com>
2015-03-06 23:48:56 +0800

    add a new brdanch

--- hello.c
+++ hello.c
@@ -1,12 +1,17 @@
 //my first program code in vim.
 
 #include <iostream>
+#include <functional>
 using namespace std;
 
+int check(int a, int b)
+{
+	return a > b ? 1 : 0;
+}
 int main()
 {
 	cout << "Hello world!" << endl;
 	//intend to say hello world and test in git.
-
+	auto check4 = bind(check, _1, _2);
 	reutrn 0;
 }
